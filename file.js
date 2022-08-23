ac4d0017c9419b491d627f4d74b1363356559e1d
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-08-23 18:02:32 +0530

    feat : sum implemented

--- file.js
+++ file.js
@@ -2,6 +2,22 @@ const dateOfBirth =document.querySelector("#date-of-birth");
 const luckyNumber =document.querySelector("#lucky-number");
 const checkNumberButton =document.querySelector("#check-number")
 
+function checkNumberIsLucky()
+{
+    const date=dateOfBirth.value;
+    const sum=caculateSum(date);
+}
+
+function caculateSum(dob)
+{
+    dob=dob.replaceAll("-","")
+    let sum=0;
+    for(let i =0;i<dob.length;i++)
+    {  sum=sum+ Number(dob.charAt(i));}
+    return sum;
+
+}
+
 checkNumberButton.addEventListener('click',function getValues(){
 
     console.log(dateOfBirth.value,luckyNumber.value);
